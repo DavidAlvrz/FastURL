517b1c9a1966c5ed54161e189e94c067dd6be1a1
David Álvarez <davidalvarezm95@gmail.com>
2024-07-29 22:06:25 +0200

    main title icon

--- webapp/src/app/navbar/navbar.component.ts
+++ webapp/src/app/navbar/navbar.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBolt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   template: `
     <nav>
-      <h1 class="main-title"> Fast URL </h1>
+      <h1 class="main-title"> <fa-icon [icon]="fabolt"></fa-icon> Fast URL </h1>
       <h6 class="subtitle"> Shorten your URLs for free, no ads included!</h6>
     </nav>
   `
 })
 export class NavbarComponent {
-
+  fabolt = faBolt;
 }
