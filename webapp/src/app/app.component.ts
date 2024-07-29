c99d79ea2907d21a97706892b76a0a1deedad704
David Álvarez <davidalvarezm95@gmail.com>
2024-07-29 23:57:05 +0200

    Refactor form validation

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -56,7 +56,7 @@ export class AppComponent {
     this.loading = true;
 
     //Validate form
-    const formOk = this.validateForm();
+    const formOk = await this.validateForm();
     if (!formOk){
       this.loading = false;
       return;
@@ -70,14 +70,6 @@ export class AppComponent {
     if (this.customURLField.show) {
       //Get custom ID
       id = this.customURLField.value.replace(this.baseUrl, '');
-
-      //Check if custom URL is being used
-      const response = await this.urlsService.getUrl(id);
-      if (response) {
-        this.customURLField.error = 'URL already in use';
-        this.loading = false;
-        return;
-      }
     }
     else {
       //Generate random ID
@@ -103,24 +95,48 @@ export class AppComponent {
     this.shortenedlURLField.value = this.baseUrl + id;
   }
 
-  validateForm() {
+  async validateForm() {
 
+    //Check if URL is empty
     if (this.originalURLField.value === '') {
       this.originalURLField.error = 'Enter an URL to shorten';
       return false;
     }
 
+    //Check if URL is valid
     const urlRegex = /^https:\/\/(www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(\/[a-zA-Z0-9\-_\/]*)?(\?[a-zA-Z0-9\-_.=&]*)?$/;
     if(!urlRegex.test(this.originalURLField.value)){
       this.originalURLField.error = 'Enter a valid URL';
       return false;
     }
 
-
+    //Check if user is to use an empty custom URL
     if (this.customURLField.show && this.customURLField.value === this.baseUrl) {
       this.originalURLField.error = '';
       this.customURLField.error = 'Enter your custom URL';
       return false;
+    }
+
+    //Check if custom URL is valid and available
+    if (this.customURLField.show) {
+
+      //Get custom ID
+      const id = this.customURLField.value.replace(this.baseUrl, '');
+
+      //Check if custom URL is valid
+      const idRegex = /^[a-zA-Z0-9_-]{2,}$/;
+      if (!idRegex.test(id)) {
+        this.customURLField.error = 'Enter a valid custom URL';
+        return false;
+      }
+
+      //Check if custom URL is already in use 
+      const response = await this.urlsService.getUrl(id);
+      if (response) {
+        this.customURLField.error = 'URL already in use';
+        this.loading = false;
+        return false;
+      }
     }
 
     return true;
