e97efa4ca12551b84ec79db8fe5f7a5a193523bb
David Álvarez <davidalvarezm95@gmail.com>
2024-07-29 12:29:49 +0200

    Added animations and custom field

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,19 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [CommonModule,RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   
   showResultField: boolean = false;
+  showCustomField: boolean = false;
 
   shortenURL() {
     this.showResultField = true;
   }
+
+  toggleCustomField() {
+    this.showCustomField = !this.showCustomField;
+  } 
+
+  baseUrl = 'www.fasturl.com/';
+  customUrl = this.baseUrl;
+
+  handleCustomInputChange(event: any) {
+    const text = event.target.value;
+    if(text.length < this.baseUrl.length)
+      event.target.value = this.baseUrl;
+  }
+
 
 }
